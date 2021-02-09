b0183d509ddf372ec3735d05046e2fdd2e0a17bc
XyLe-GBP <kawakami.t.1953@gmail.com>
2021-02-09 17:54:44 +0900

    version 1.5 source

---
+++ v1.5/waifu2x-ncnn-vulkan/DOWNLOADDLG.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// DOWNLOADDLG ダイアログ
+
+class DOWNLOADDLG : public CDialogEx
+{
+	DECLARE_DYNAMIC(DOWNLOADDLG)
+
+public:
+	DOWNLOADDLG(CWnd* pParent = nullptr);   // 標準コンストラクター
+	virtual ~DOWNLOADDLG();
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DOWNLOAD };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
+
+	DECLARE_MESSAGE_MAP()
+};
